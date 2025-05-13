172ede78d0762f4c9f9a9b79a0438ebbf0f613dc
adosun123 <adoram1982@hotmail.com>
2025-05-13 16:13:46 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,26 @@
-import { useEffect } from 'react';
-import Script from 'next/script';
-
-const GA_MEASUREMENT_ID = 'G-PYRX4X3TPM';
-
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window.dataLayer = window.dataLayer || [];
-      function gtag() { window.dataLayer.push(arguments); }
-      gtag('js', new Date());
-      gtag('config', GA_MEASUREMENT_ID);
-    }
-  }, []);
-
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-      />
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-PYRX4X3TPM"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PYRX4X3TPM');
+            `,
+          }}
+        ></script>
+      </head>
       <Component {...pageProps} />
     </>
   );
 }
 
 export default MyApp;
+
